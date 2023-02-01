51c70f435507fcff29afe6dd156e5192be577f3a
Thoriq Daffa <mnurula.nu246@gmail.com>
2023-02-01 21:17:55 +0700

    Adding some view for update

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -27,10 +27,30 @@
 	<main>
 		<section class="main-content">
 			<div class="container">
-				<div class="d-flex justify-content-start">
-					<p>asdasd</p>
-					<p>asdasd</p>
-					<p>asdasd</p>
+				<div class="slider mt-5">
+					<div class="d-flex justify-content-center">
+						<div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img src="assets\image\default.jpg" class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+									<img src="assets\image\default.jpg" class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+									<img src="assets\image\default.jpg" class="d-block w-100" alt="...">
+								</div>
+							</div>
+							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Previous</span>
+							</button>
+							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Next</span>
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
