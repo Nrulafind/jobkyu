d40caea16dbd8d5689a6e8d4efa10835f2b4e232
Thoriq Daffa <mnurula.nu246@gmail.com>
2023-02-01 20:58:45 +0700

    add view navbar

---
+++ application/views/homepage.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>JobQ</title>
+
+	<!-- CSS File -->
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+	<!-- JS File -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+	<!-- Navbar Start -->
+	<?php
+	$this->load->view('template/navbar');
+	?>
+	<!-- Navbar End -->
+
+	<!-- Main body start -->
+
+	<main>
+		<section class="main-content">
+			<div class="container">
+				<div class="d-flex justify-content-start">
+					<p>asdasd</p>
+					<p>asdasd</p>
+					<p>asdasd</p>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<!-- Main body end -->
+
+</body>
+
+</html>
