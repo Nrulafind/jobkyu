30a16105ac4dbe2c2d3502153940b904aa142a4a
Thoriq Daffa <78408504+thordaff@users.noreply.github.com>
2023-02-03 01:40:15 +0700

    Adding css file and views file for admin side

---
+++ application/views/template/admin/sidebar.php
@@ -0,0 +1,25 @@
+<div class="sidebar position-fixed">
+    <div class="sidebar-title">
+        <h2 class="text-light" style="margin-left: 25%; margin-top: 20px;">JobQ</h2>
+        <hr style="color: white;">
+    </div>
+    <div class="sidebar-body text-light">
+        <ul>
+            <li>
+                <a href="#"><i class="fa-solid fa-gauge" style="margin-right: 20px;"></i>Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-handshake" style="margin-right: 20px;"></i>Partners</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-newspaper" style="margin-right: 20px;"></i>Career articles</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa-solid fa-circle-info" style="margin-right: 20px;"></i>General Information</a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-footer ms-4">
+        <a href="#" class="text-light" style="text-decoration: none;"><i class="fa-solid fa-right-from-bracket logout" style="margin-right: 20px;"></i>LogOut</a>
+    </div>
+</div>
