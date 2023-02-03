6c3ca895a64122d279e52aea9d8d472236bf7797
Thoriq Daffa <mnurula.nu246@gmail.com>
2023-02-03 16:36:56 +0700

    add dummy code for auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,7 +7,7 @@ class Auth extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		// $this->load->model('M_auth');
+		$this->load->model('M_auth');
 	}
 	//login 
 	public function login()
@@ -20,19 +20,42 @@ class Auth extends CI_Controller
 		$u = $this->input->post('username');
 		$p = $this->input->post('password');
 
-		$cek = $this->Mlogin->cek_login($u, $p);
+		$cek = $this->M_auth->cek_login($u, $p);
+		$cek_role = $this->M_auth->cek_role($u, $p);
 
 		if ($cek) {
-			$data_session = [
-				'idAdmin' => $cek['idAdmin'],
-				'userName' => $cek['userName'],
-				'status' => 'login'
-			];
+			if ($cek_role['id_role'] == 1) {
+				$data_session = [
+					'username' => $cek['username'],
+					'password' => $cek['password'],
+					'status' => 'login'
+				];
 
-			$this->session->set_userdata($data_session);
-			redirect('adminpanel/dashboard');
+				$this->session->set_userdata($data_session);
+				redirect('');
+			}
+			if ($cek_role['id_role'] == 2) {
+				$data_session = [
+					'username' => $cek['username'],
+					'password' => $cek['password'],
+					'status' => 'login'
+				];
+
+				$this->session->set_userdata($data_session);
+				redirect('');
+			}
+			if ($cek_role['id_role'] == 3) {
+				$data_session = [
+					'username' => $cek['username'],
+					'password' => $cek['password'],
+					'status' => 'login'
+				];
+
+				$this->session->set_userdata($data_session);
+				redirect('');
+			}
 		} else {
-			redirect('adminpanel');
+			redirect('');
 		}
 	}
 
@@ -41,10 +64,62 @@ class Auth extends CI_Controller
 	{
 		$this->load->view('auth/register');
 	}
+	//function aksi untuk register data mitra ke database
+	public function aksi_daftar_mitra()
+	{
+		$username = $this->input->post('username');
+		$mitra = $this->M_auth->cek_user($username);
 
+		if ($mitra) {
+			$this->session->set_flashdata('error', '<b>Username</b> ini sudah terdaftar!');
+			redirect('');
+		} else {
+			$data = [
+				'username' => $this->input->post('username'),
+				'password' => $this->input->post('password'),
+				'namaKonsumen' => $this->input->post('namaKonsumen'),
+				'alamat' => $this->input->post('alamat'),
+				'idKota' => $this->input->post('idKota'),
+				'email' => $this->input->post('email'),
+				'tlpn' => $this->input->post('tlpn'),
+				'id_role' => 1
+			];
+
+			$this->M_auth->insert_mitra('tbl_user', $data);
+			$this->session->set_flashdata('success', '<b>Berhasil</b>. Akun Anda telah terdaftar');
+			redirect('');
+		}
+	}
+	//function aksi untuk register data user ke database
+	public function aksi_daftar_user()
+	{
+		$username = $this->input->post('username');
+		$user = $this->M_auth->cek_user($username);
+
+		if ($user) {
+			$this->session->set_flashdata('error', '<b>Username</b> ini sudah terdaftar!');
+			redirect('');
+		} else {
+			$data = [
+				'username' => $this->input->post('username'),
+				'password' => $this->input->post('password'),
+				'namaKonsumen' => $this->input->post('namaKonsumen'),
+				'alamat' => $this->input->post('alamat'),
+				'idKota' => $this->input->post('idKota'),
+				'email' => $this->input->post('email'),
+				'tlpn' => $this->input->post('tlpn'),
+				'id_role' => 2
+			];
+
+			$this->M_auth->insert_user('tbl_user', $data);
+			$this->session->set_flashdata('success', '<b>Berhasil</b>. Akun Anda telah terdaftar');
+			redirect('');
+		}
+	}
+	//untuk melakukan destroy session
 	public function logout()
 	{
 		$this->session->sess_destroy();
-		redirect('adminpanel');
+		redirect('');
 	}
 }
