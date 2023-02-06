825f70726dbc846da93002818b11ff0676f35549
Thoriq Daffa <mnurula.nu246@gmail.com>
2023-02-06 19:03:04 +0700

    penyesuaian dummy code tips karir

--- application/controllers/Karir.php
+++ application/controllers/Karir.php
@@ -11,56 +11,109 @@ class Karir extends CI_Controller
 	//index function apply tips_karir
 	public function index()
 	{
-		$data["tips_karir"] = $this->M_mitra->getAll();
+		$data["tips_karir"] = $this->M_karir->getAll();
 		$this->load->view("", $data);
 	}
-	//function untuk menambahkan tips_karir oleh mitra
+	//function untuk menambahkan tips_karir oleh tips_karir
 	public function add_tips_karir()
 	{
 		if ($this->session->status != '') {
 			redirect('');
 		}
-		$nama = $this->input->post('nama');
-		$mapel = $this->input->post('mapel');
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$data = array(
-			'nama' => $nama,
-			'mapel' => $mapel,
-			'username' => $username,
-			'password' => $password
-		);
-		$this->M_mitra->insert_tips_karir('tbl_tips_karir', $data);
-		redirect('');
+		$judul = $this->input->post('');
+		$materi = $this->input->post('');
+		$tgl_rilis = $this->input->post('');
+		$media = $_FILES['media'];
+
+		if (empty($media['name'])) {
+			$data = array(
+				'judul' => $judul,
+				'materi' => $materi,
+				'tgl_rilis' => $tgl_rilis,
+			);
+			$this->M_karir->insert_tips_karir('tbl_tips_karir', $data);
+			$this->session->set_flashdata('tips_karir', 'tips_karir berhasil ditambahkan');
+			redirect('');
+		} else {
+			$config['upload_path'] = './../media';
+			$config['allowed_types'] = 'jpg|png|pdf';
+			//load library upload
+			$this->load->library('upload', $config);
+			//proses upload file
+			if (!$this->upload->do_upload('media')) {
+				$data['error'] = $this->upload->display_errors();
+				redirect('', $data);
+			} else {
+				$media = $this->upload->data('file_name');
+				$data = array(
+					'judul' => $judul,
+					'materi' => $materi,
+					'tgl_rilis' => $tgl_rilis,
+					'media' => $media
+				);
+
+				$this->M_karir->insert_tips_karir_file('tbl_tips_karir', $data);
+				$this->session->set_flashdata('tips_karir', 'tips_karir berhasil ditambahkan');
+				redirect('');
+			}
+		}
 	}
-	//function untuk melakukan edit tips_karir oleh mitra
+	//function untuk melakukan edit tips_karir oleh tips_karir
 	public function edit_tips_karir($id)
 	{
-		if ($this->session->status != 'admin') {
+		if ($this->session->status != '') {
 			redirect('');
 		}
-		$nama = $this->input->post('nama');
-		$mapel = $this->input->post('mapel');
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$where = array('' => $id);
-		$data = array(
-			'nama' => $nama,
-			'mapel' => $mapel,
-			'username' => $username,
-			'password' => $password
-		);
-		$this->M_mitra->update_tips_karir($where, '', $data);
-		redirect('');
+		$judul = $this->input->post('');
+		$materi = $this->input->post('');
+		$tgl_rilis = $this->input->post('');
+		$media = $_FILES['media'];
+
+		$where = ['id_tips_karir' => $id];
+
+		if (empty($media['name'])) {
+			$data = array(
+				'judul' => $judul,
+				'materi' => $materi,
+				'tgl_rilis' => $tgl_rilis,
+			);
+			$this->M_karir->insert_tips_karir($where, 'tbl_tips_karir', $data);
+			$this->session->set_flashdata('tips_karir', 'tips_karir berhasil diubah');
+			redirect('');
+		} else {
+			$s = $this->db->query('select media from tbl_tips_karir where id_tips_karir=' . $id)->row();
+			unlink('./../media/' . $s->media);
+			$config['upload_path'] = './../media';
+			$config['allowed_types'] = 'jpg|png|pdf';
+			//load library upload
+			$this->load->library('upload', $config);
+			//proses upload file
+			if (!$this->upload->do_upload('media')) {
+				$data['error'] = $this->upload->display_errors();
+				redirect('', $data);
+			} else {
+				$media = $this->upload->data('file_name');
+				$data = array(
+					'judul' => $judul,
+					'materi' => $materi,
+					'tgl_rilis' => $tgl_rilis,
+					'media' => $media
+				);
+
+				$this->M_karir->insert_tips_karir_file($where, 'tbl_tips_karir', $data);
+				$this->session->set_flashdata('tips_karir', 'tips_karir berhasil diubah');
+				redirect('');
+			}
+		}
 	}
-	//function untuk melakukan hapus tips_karir oelh mitra
-	public function delete($id)
+	//function untuk melakukan hapus tips_karir oelh tips_karir
+	public function delete_tips_karir($id)
 	{
-		if ($this->session->status != 'admin') {
+		if ($this->session->status != '') {
 			redirect('');
 		}
-		$where = array('' => $id);
-		$this->M_mitra->delete_tips_karir($where, '');
+		$where = array('tbl_tips_karir' => $id);
+		$this->M_karir->delete_tips_karir($where, 'tbl_tips_karir');
 		redirect('');
 	}
 }
--- application/models/M_karir.php
+++ application/models/M_karir.php
@@ -5,18 +5,34 @@ class M_karir extends CI_Model
 {
 	public function getAll()
 	{
-		return $this->db->get($this->_table)->result();
+		$id = $this->session->userdata('email');
+		$this->db->join('tbl_user', 'tbl_user.id_user = tbl_tips_karir.id_user');
+		$this->db->where('email', $id);
 	}
 
+	//function tips_karir
+	//function untuk insert data tips_karir
 	function insert_tips_karir($table, $data)
 	{
 		$this->db->insert($table, $data);
 	}
-	function update_tips_karir($where, $table, $data)
+	function insert_tips_karir_file($table, $data)
+	{
+		$this->db->insert($table, $data);
+	}
+	//function untuk update data tips_karir
+	function edit_tips_karir($where, $table, $data)
 	{
 		$this->db->where($where);
 		$this->db->update($table, $data);
 	}
+	function edit_tips_karir_file($where, $table, $data)
+	{
+		$this->db->where($where);
+		$this->db->update($table, $data);
+	}
+	//function untuk delete inputan tips_karir
+	//function untuk delete tips_karir
 	function delete_tips_karir($where, $table)
 	{
 		$this->db->where($where);
